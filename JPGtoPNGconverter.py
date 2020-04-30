5031ea26a926768d0250a488e732950a5bade721
firdavsxon <akilovfs@gmail.com>
2020-04-30 12:55:51 -0700

    printing

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -19,3 +19,4 @@ for filename in os.listdir(image_folder):
 	img.save(f'{dest_folder}{clean_name}.png', 'png')
 	x.save(f'{dest_folder}{clean_name}.enh.png', 'png')
 	print('all done!')
+	print("cheking for branch")
